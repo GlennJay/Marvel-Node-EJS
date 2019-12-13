d8270478bd7a616b7cb1fa0902ff49dc19d5f742
Jason Glenn <glennwjason@gmail.com>
2019-12-13 00:37:21 -0500

    finished key verification

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 var api = require('marvel-api');
-require('dotenv').config({path:'Variables.env'});
+var marvelKey = require('./marvel')
 var crypto = require('crypto');
 
 var timeStamp = new Date().getTime();
-var marvelHash = crypto.createHash('md5').update(timeStamp + privKey + pubKey).digest('hex');
+var marvelHash = crypto.createHash('md5').update(timeStamp + marvelKey.privKey + marvelKey.pubKey).digest('hex');
 
 
 var charactersURL = 'https://gateway.marvel.com:443/v1/public/characters?name=wolverine&apikey=63e5e013e5e1b67f47366be462ccc44c'
 var marvel = api.createClient({
-    publicKey: pubKey
-  , privateKey: privKey
+    publicKey: marvelKey.pubKey
+  , privateKey: marvelKey.privKey
   });
 
 /* GET home page. */
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
  axios.get('https://gateway.marvel.com:443/v1/public/characters?name=wolverine',{
   params: {
     ts:  timeStamp,
-    apikey: pubKey,
+    apikey: marvelKey.pubKey,
     hash:  marvelHash
    }
 })
